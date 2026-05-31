5c28a83de726f8fcb27395779385bb09eb38b1ee
naniiic137 <hamza.benismail.6@gmail.com>
2026-05-31 23:14:39 +0100

    Commit firebase-config.js for Netlify deployment

---
+++ firebase-config.js
@@ -0,0 +1,12 @@
+// ===== FIREBASE CONFIGURATION =====
+// Paste your Firebase project config here (the config object only)
+const firebaseConfig = {
+  apiKey: "AIzaSyD5mjir_ug6ZQDgzRQHYiihmUj001fzFhw",
+  authDomain: "chkobba-fb211.firebaseapp.com",
+  databaseURL: "https://chkobba-fb211-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "chkobba-fb211",
+  storageBucket: "chkobba-fb211.firebasestorage.app",
+  messagingSenderId: "612428857593",
+  appId: "1:612428857593:web:e4d1ea746dea1fb9d42228",
+  measurementId: "G-VT04LX0QF1"
+};
